a509d192c185b22b9fc9cce7681311291071e397
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2023-12-27 15:39:35 +0100

    Day 21 part 2

--- 2023/day21/part2.py
+++ 2023/day21/part2.py
@@ -1,55 +1,66 @@
-import copy
+from typing import TypeAlias
+from collections import deque
 
-
-def print_grid(grid: list[list[str]], mark: set[tuple[int, int]]):
-    grid = copy.deepcopy(grid)
-    for reach in mark:
-        grid[reach[1]][reach[0]] = "O"
-    print("----------------")
-    print("\n".join(["".join(row) for row in grid]))
+Coord: TypeAlias = tuple[int, int]
 
 
 def main():
-    total_steps: int = 1000
-    total_steps_is_even = total_steps % 2 == 0
-    with open("example.txt", "r") as file:
+    steps: int = 26501365
+    assert steps % 2 == 1
+
+    with open("input.txt", "r") as file:
         grid = [list(line.strip()) for line in file.readlines()]
-        size_x = len(grid[0])
-        size_y = len(grid)
+        start = (len(grid) // 2, len(grid) // 2)
+        assert len(grid[0]) == len(grid)  # grid is square
+        assert grid[start[1]][start[0]] == "S"  # starting point is in the middle
 
-        step = 0
-        reachable = 1  # starting point
-        border: set[tuple[int, int]] = set()
-        prev_border: set[tuple[int, int]] = set()
-
-        for y in range(size_y):
-            for x in range(size_x):
-                if grid[y][x] == "S":
-                    border.add((x, y))
-
-        while step < total_steps:
-            new_border: set[tuple[int, int]] = set()
-            for tile in border:
-                for neighbour in [
-                    (tile[0] + 1, tile[1]),
-                    (tile[0] - 1, tile[1]),
-                    (tile[0], tile[1] + 1),
-                    (tile[0], tile[1] - 1),
-                ]:
+        distances: dict[Coord, int] = {}
+        border: deque[tuple[int, Coord]] = deque([(0, start)])
+        while len(border) > 0:
+            distance, coord = border.popleft()
+            if coord in distances:
+                continue
+            distances[coord] = distance
+            for neighbour in [
+                (coord[0] + 1, coord[1]),
+                (coord[0] - 1, coord[1]),
+                (coord[0], coord[1] + 1),
+                (coord[0], coord[1] - 1),
+            ]:
+                if (
+                    neighbour[0] >= 0
+                    and neighbour[0] < len(grid)
+                    and neighbour[1] >= 0
+                    and neighbour[1] < len(grid)
+                ):
                     if (
-                        neighbour not in new_border
-                        and neighbour not in border
-                        and neighbour not in prev_border
-                        and grid[neighbour[1] % size_y][neighbour[0] % size_x] != "#"
+                        neighbour not in distances
+                        and grid[neighbour[1]][neighbour[0]] != "#"
                     ):
-                        new_border.add(neighbour)
-                        if step % 2 == total_steps_is_even:
-                            reachable += 1
-            prev_border = border
-            border = new_border
-            step += 1
+                        border.append((distance + 1, neighbour))
 
-        print(reachable)
+        assert steps == 131 * 202300 + 65
+        assert len(grid) == 2 * 65 + 1
+
+        grids = 202300
+        even_grid_count = pow(grids, 2)
+        odd_grid_count = pow(grids + 1, 2)
+        negative_odd_corners = grids + 1
+        positive_even_corners = grids
+
+        even = sum(map(lambda d: d % 2 == 0, distances.values()))
+        odd = sum(map(lambda d: d % 2 == 1, distances.values()))
+        even_corner = sum(map(lambda d: d % 2 == 0 and d > 65, distances.values()))
+        odd_corner = sum(map(lambda d: d % 2 == 1 and d > 65, distances.values()))
+
+        result = (
+            even_grid_count * even
+            + odd_grid_count * odd
+            - negative_odd_corners * odd_corner
+            + positive_even_corners * even_corner
+        )
+
+        print(result)
 
 
 if __name__ == "__main__":
