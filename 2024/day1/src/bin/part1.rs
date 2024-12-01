89f6b78ee4f83f804f5e263b5ba7529f3323ad1a
Lukas Dobbelsteen <l.dobbelsteen@gmail.com>
2024-12-01 09:42:44 +0100

    Day 1

---
+++ 2024/day1/src/bin/part1.rs
@@ -0,0 +1,39 @@
+#![warn(clippy::pedantic)]
+
+use anyhow::{anyhow, Result};
+
+fn main() -> Result<()> {
+    let file = std::fs::read_to_string("input.txt")?;
+
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = file
+        .lines()
+        .map(|line| {
+            let (a, b) = line
+                .split_once("   ")
+                .ok_or(anyhow!("invalid line: {line}"))?;
+            Ok((a.parse()?, b.parse()?))
+        })
+        .collect::<Result<Vec<(i32, i32)>>>()?
+        .into_iter()
+        .unzip();
+
+    if left.len() != right.len() {
+        return Err(anyhow!(
+            "list size mismatch: left={} right={}",
+            left.len(),
+            right.len()
+        ));
+    }
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    let mut result = 0;
+    for (a, b) in left.iter().zip(right.iter()) {
+        result += (a - b).abs();
+    }
+
+    println!("result: {result}");
+
+    Ok(())
+}
---
+++ 2024/day1/src/bin/part2.rs
@@ -0,0 +1,45 @@
+#![warn(clippy::pedantic)]
+
+use anyhow::{anyhow, Result};
+
+fn main() -> Result<()> {
+    let file = std::fs::read_to_string("input.txt")?;
+
+    let (left, right): (Vec<i32>, Vec<i32>) = file
+        .lines()
+        .map(|line| {
+            let (a, b) = line
+                .split_once("   ")
+                .ok_or(anyhow!("invalid line: {line}"))?;
+            Ok((a.parse()?, b.parse()?))
+        })
+        .collect::<Result<Vec<(i32, i32)>>>()?
+        .into_iter()
+        .unzip();
+
+    if left.len() != right.len() {
+        return Err(anyhow!(
+            "list size mismatch: left={} right={}",
+            left.len(),
+            right.len()
+        ));
+    }
+
+    let right_occurrences = right
+        .iter()
+        .fold(std::collections::HashMap::new(), |mut acc, &x| {
+            *acc.entry(x).or_insert(0) += 1;
+            acc
+        });
+
+    let mut result = 0;
+    for v in &left {
+        if let Some(&count) = right_occurrences.get(v) {
+            result += v * count;
+        }
+    }
+
+    println!("result: {result}");
+
+    Ok(())
+}
