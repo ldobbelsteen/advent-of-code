8640f8d4256f6d0c6d6beab99f81343d4bb06b76
Lukas <lukas.dobbelsteen@essent.nl>
2025-12-08 20:33:21 +0100

    Day 4

---
+++ 2025/src/bin/4a.rs
@@ -0,0 +1,74 @@
+#![warn(clippy::pedantic)]
+
+use anyhow::{anyhow, Result};
+
+fn surrounding_count(grid: &[Vec<bool>], i: usize, j: usize) -> Result<usize> {
+    let directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    let mut count = 0;
+    for (di, dj) in directions {
+        let ni = (i as isize) + di;
+        let nj = (j as isize) + dj;
+
+        if ni < 0 || nj < 0 {
+            continue;
+        }
+
+        if let Some(row) = grid.get(ni as usize) {
+            if let Some(&cell) = row.get(nj as usize) {
+                if cell {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+fn is_reachable(grid: &[Vec<bool>], i: usize, j: usize) -> Result<bool> {
+    let count = surrounding_count(grid, i, j)?;
+    Ok(count < 4)
+}
+
+fn main() -> Result<()> {
+    let file = std::fs::read_to_string("inputs/4-input.txt")?;
+
+    let grid = file
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| {
+                    if c == '@' {
+                        Ok(true)
+                    } else if c == '.' {
+                        Ok(false)
+                    } else {
+                        Err(anyhow!("invalid char: {c}"))
+                    }
+                })
+                .collect::<Result<Vec<bool>>>()
+        })
+        .collect::<Result<Vec<Vec<bool>>>>()?;
+
+    let mut result = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] && is_reachable(&grid, i, j)? {
+                result += 1;
+            }
+        }
+    }
+    println!("{result}");
+
+    Ok(())
+}
---
+++ 2025/src/bin/4b.rs
@@ -0,0 +1,92 @@
+#![warn(clippy::pedantic)]
+
+use anyhow::{anyhow, Result};
+
+fn surrounding_count(grid: &[Vec<bool>], i: usize, j: usize) -> Result<usize> {
+    let directions = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    let mut count = 0;
+    for (di, dj) in directions {
+        let ni = (i as isize) + di;
+        let nj = (j as isize) + dj;
+
+        if ni < 0 || nj < 0 {
+            continue;
+        }
+
+        if let Some(row) = grid.get(ni as usize) {
+            if let Some(&cell) = row.get(nj as usize) {
+                if cell {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    Ok(count)
+}
+
+fn is_reachable(grid: &[Vec<bool>], i: usize, j: usize) -> Result<bool> {
+    let count = surrounding_count(grid, i, j)?;
+    Ok(count < 4)
+}
+
+fn remove_reachable(grid: &mut [Vec<bool>]) -> Result<usize> {
+    let mut to_remove = vec![];
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] && is_reachable(grid, i, j)? {
+                to_remove.push((i, j));
+            }
+        }
+    }
+
+    let count = to_remove.len();
+    for (i, j) in to_remove {
+        grid[i][j] = false;
+    }
+
+    Ok(count)
+}
+
+fn main() -> Result<()> {
+    let file = std::fs::read_to_string("inputs/4-input.txt")?;
+
+    let mut grid = file
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| {
+                    if c == '@' {
+                        Ok(true)
+                    } else if c == '.' {
+                        Ok(false)
+                    } else {
+                        Err(anyhow!("invalid char: {c}"))
+                    }
+                })
+                .collect::<Result<Vec<bool>>>()
+        })
+        .collect::<Result<Vec<Vec<bool>>>>()?;
+
+    let mut result = 0;
+    loop {
+        let removed = remove_reachable(&mut grid)?;
+        if removed == 0 {
+            break;
+        }
+        result += removed;
+    }
+    println!("{result}");
+
+    Ok(())
+}
